0b9d3fe082b01d94a499608d3027d77f01251514
TayyabShaban <66559710+TayyabShaban@users.noreply.github.com>
2020-06-07 22:22:26 +0500

    quiz1

--- quiz1.go
+++ quiz1.go
@@ -1,6 +1,25 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	var no int
+	for {
+		fmt.Println("enter no 1-4 for country details 0 to quit")
+		_, _ = fmt.Scanf("%d", &no)
+		if no == 0 {
+			break
+		} else if no == 1 {
+			fmt.Println("Corona cases in Pakistan = 98,943")
+		} else if no == 2 {
+			fmt.Println("Corona cases in South Korea = 11,776")
+		} else if no == 3 {
+			fmt.Println("Corona cases in France = 154,000")
+		} else if no == 4 {
+			fmt.Println("Hello, Corona! ")
+		}
+
+	}
 }
