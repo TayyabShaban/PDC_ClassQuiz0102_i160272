da977f1684857b0be854595afde306597398d159
TayyabShaban <66559710+TayyabShaban@users.noreply.github.com>
2020-06-07 23:10:40 +0500

    quiz1

--- quiz2.go
+++ quiz2.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"Quiz1pkg"
+	"fmt"
+)
+
+func main() {
+	var no int
+	for {
+		fmt.Println("enter no 1-4 for country details 0 to quit")
+		_, _ = fmt.Scanf("%d", &no)
+		var ans string = Quiz1pkg.PrintCoronaDetails(no)
+		if ans == "break" {
+			break
+		} else {
+			fmt.Print(ans)
+		}
+
+	}
+
+}
